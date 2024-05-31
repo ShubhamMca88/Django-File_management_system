4a65a02850eb69f89715a897de5404e5c1219288
Shubham <byshubham6@gmail.com>
2024-05-31 19:19:01 +0530

    #

--- conf/settings.py
+++ conf/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'blog',
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,4 @@
+from django.urls import path
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 
